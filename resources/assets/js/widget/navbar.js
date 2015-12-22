3ed68eeee6cbe6db3a0ffc8e53e4419a62cf42a5
Michael Conroy <michael@prometheusresearch.com>
2015-12-22 18:08:53 -0500

    Added more module import name fixes

--- resources/assets/js/widget/navbar.js
+++ resources/assets/js/widget/navbar.js
@@ -1,7 +1,7 @@
 'use strict';
 
 import React from 'react';
-import NavTile from './NavTile';
+import NavTile from './navtile';
 import {Navbar, Nav, NavItem} from 'react-bootstrap';
 
 export default class NavBar extends React.Component {
